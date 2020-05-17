31acb50b5de0efa4f82c779e2394cf4834d5da9a
Fabio Pozzi <pozzi.fabio@gmail.com>
2020-05-17 23:24:20 +0200

    engine generates geojson data from gpx. WIP: use mapboxgl to render full page map.

--- engine.js
+++ engine.js
@@ -1,16 +1,23 @@
 const fs = require('fs')
 const path = require('path')
 const Mustache = require('mustache')
+const tj = require('@mapbox/togeojson'),
+// node doesn't have xml parsing or a dom. use xmldom
+DOMParser = require('xmldom').DOMParser;
+
+var gpx_file = new DOMParser().parseFromString(fs.readFileSync('giro.gpx', 'utf8'));
+var converted = tj.gpx(gpx_file);
 
 const object_to_render = {
     sidebar: [
         { "link": "about.html", "title": "About" },
         { "link": "bici.html", "title": "Bici" }
-    ]
+    ],
+    gpx: JSON.stringify(converted)
 }
 
 // lista dei file
-const lista_file = [ 'index', 'about' ]
+const lista_file = [ 'index', 'about', 'bici', 'linux' ]
 
 lista_file.forEach( item => {
     const input_file = path.join('templates', item + '.ms')
@@ -25,3 +32,4 @@ lista_file.forEach( item => {
         console.log('File ' + output_file + ' saved!');
     })
 })
+
