3ee722b589ea55dbd19a0a65fae410af21a8add5
Fabio Pozzi <fabio@fabiopozzi.net>
2020-05-28 01:07:56 +0200

    Improve utils tests: add test for file copy

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -7,7 +7,7 @@ const destFolder = path.join('TestContent','css')
 it('tries to create folder if doesn\'t exists', () => {
     fs.existsSync = jest.fn().mockReturnValue(false);
     fs.mkdirSync = jest.fn();
-    utils.createCSSFolder(destFolder)
+    utils.createCSSFolder(destFolder);
     // The argument of the first call to the function was destFolder
     expect(fs.mkdirSync).toHaveBeenCalledWith(destFolder);
 });
@@ -15,8 +15,15 @@ it('tries to create folder if doesn\'t exists', () => {
 it('prints a message if folder exists', () => {
     fs.mkdirSync = jest.fn();
     fs.existsSync = jest.fn().mockReturnValue(true);
-    console.log = jest.fn()
-    utils.createCSSFolder(destFolder)
+    console.log = jest.fn();
+    utils.createCSSFolder(destFolder);
     // The argument of the first call to the function was destFolder
-    expect(console.log).toHaveBeenCalledWith('directory esistente')
+    expect(console.log).toHaveBeenCalledWith('directory esistente');
 });
+
+it('copy static file into destination folder', () => {
+    fs.copyFileSync = jest.fn();
+    fs.readdirSync = jest.fn().mockReturnValue(['prova.css']);
+    utils.copyCSSFiles('/tmp/css', 'content/css')
+    expect(fs.copyFileSync).toHaveBeenCalledWith('/tmp/css/prova.css', 'content/css/prova.css');
+})
